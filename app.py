4072e104c317510912299f991c5a783147172e2d
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 13:19:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template,request
+from keras.models import load_model
+from keras.models import model_from_json
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 @app.route("/")
@@ -9,7 +13,44 @@ def home():
 def after():
     img=request.files['file1']
     img.save('./static/file.jpg')
-    return render_template('after.html')
+    image=cv2.imread('./static/file.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
+    faces = cascade.detectMultiScale(gray, 1.1, 3)
+    
+    for x,y,w,h in faces:
+      cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+      cropped=image[y:y+h,x:x+w]
+    cv2.imwrite('./static/after.jpg', image)
+    
+    try:
+      cv2.imwrite('./static/cropped.jpg',cropped)
+    except:
+      pass
+    
+    try:
+      image = cv2.imread('./static/cropped.jpg', 0)
+    except:
+      image = cv2.imread('./static/file.jpg', 0)
+     
+    image = cv2.resize(image, (48,48))
+    image = image/255.0
+    
+    image = np.reshape(image, (1,48,48,1))
+    
+    model = model_from_json(open("./model.json", "r").read())
+    
+    model.load_weights('/content/modelweights.h5')
+    
+    prediction = model.predict(image)
+    
+    label_map =   ['Anger','Disgust','Fear', 'Happy', 'Sad', 'Surprise','Neutral']
+    
+    prediction = np.argmax(prediction)
+    
+    final_prediction = label_map[prediction]
+    
+    return render_template('after.html',data=final_prediction)
  
 if __name__ == "__main__":
     app.run()

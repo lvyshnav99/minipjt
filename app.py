89e36b75a48ff18c88bc83e51639e1c560027ab7
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 12:02:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template, request
-from keras.models import load_model
-from keras.models import model_from_json
-import numpy as np
-import cv2
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/home')
 def home():
-  return render_template('base.html')
-@app.route('/after',methods=['GET','POST'])
-def after():
-  return "<h2> hii</h2>"
-    
+    return render_template('home.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

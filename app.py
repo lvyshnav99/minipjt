5e096ef9677c73f6457f5338b9c881c9f3068baf
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 12:11:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

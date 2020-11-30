1a315a76745132676e436c50d94e96153ec90c26
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 12:55:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 @app.route("/")
@@ -7,6 +7,8 @@ def home():
 
 @app.route("/after",methods=['GET','POST'])
 def after():
+    img=request.files['file1']
+    img.save('./static/file.jpg')
     return "hii"
  
 if __name__ == "__main__":

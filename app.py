77f031609de57b700d424ca5118a4680523c4cd3
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 12:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
-
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('base.html')
+  return render_template('base.html')
 
 @app.route('/after',methods=['GET','POST'])
 def after():
-    return "<h2>hello</h2>"
-    
+    return "<h2>hello world</h2>"
  
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

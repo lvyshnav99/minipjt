222a950b1e6c41d1042c30e5ad576889111a7783
lvyshnav99 <51263233+lvyshnav99@users.noreply.github.com>
2020-11-30 12:28:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 app = Flask(__name__)
+import cv2
 
 @app.route('/')
 def home():
@@ -7,7 +8,12 @@ def home():
 
 @app.route('/after',methods=['GET','POST'])
 def after():
-  img=request.files['file1']
-  return "<h2>hello world</h2>"
+    img=request.files['file1']
+    img.save('./static/file.jpg')
+    image=cv2.imread('./static/file.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return render_template('after.html')
+    
+ 
 if __name__ == "__main__":
     app.run(debug=True)
